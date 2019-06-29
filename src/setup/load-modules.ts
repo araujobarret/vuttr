48046e550eb27b652cfab8b215d7a26a27847ee2
araujobarret <araujobarret@gmail.com>
2019-06-29 01:58:29 -0300

    Adds swaggers and starts the configuration implementation

--- src/setup/load-modules.ts
+++ src/setup/load-modules.ts
@@ -2,22 +2,22 @@ import express from 'express';
 import * as cors from 'cors';
 import { json, urlencoded } from 'body-parser';
 import * as morgan from 'morgan';
+import * as swaggerUi from 'swagger-ui-express';
 
 import router from '../controllers/tools';
 import { NODE_ENV } from '../config/env';
+import * as swaggerDocument from '../config/swagger.json';
 
-// Load express modules
 export default function(server: express.Application): void {
   console.info(`INFO - loading express modules`);
   server.use(cors());
   server.use(json());
   server.use(urlencoded({ extended: true }));
 
-  // logs
   if (NODE_ENV === 'development') {
     server.use(morgan('tiny'));
   }
 
-  // Controllers/routes
+  server.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
   server.use('/', router);
 }
